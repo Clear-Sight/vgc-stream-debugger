f805ca95b82110f9c04113a1fc89282b3d66e3ba
AxelGard <axel.gard@protonmail.com>
2021-04-07 14:27:23 +0200

    revert to tcp

--- debugger/sender.py
+++ debugger/sender.py
@@ -2,9 +2,9 @@ import base64
 import cv2
 import zmq
 
-context = zmq.Context.instance()
-radio = context.socket(zmq.RADIO)
-radio.connect('udp://localhost:7777')
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.connect('tcp://localhost:7777')
 
 camera = cv2.VideoCapture(0)
 
@@ -14,7 +14,7 @@ while True:
         frame = cv2.resize(frame, (640, 480))
         encoded, buf = cv2.imencode('.jpg', frame)
         image = base64.b64encode(buf)
-        radio.send(image, group='feed')
+        socket.send(image)
     except KeyboardInterrupt:
         camera.release()
         cv2.destroyAllWindows()
